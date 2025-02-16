9b531d70cbc3e1acfad791e772cc469ca3756723
Chiragdhakad <chiragdhakad2005@gmail.com>
2025-02-16 23:11:28 +0530

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: "Smartphone X",
+    price:20000,
+    image: "./Images/image1.jpg",
+    description: "Latest smartphone with advanced features"
+  },
+  {
+    id: 2,
+    name: "Laptop Pro",
+    price: 60000,
+    image: "./Images/image2.jpg",
+    description: "Powerful laptop for professionals"
+  },
+  {
+    id: 3,
+    name: "Wireless Headphones",
+    price: 2000,
+    image: "./Images/image3.jpg",
+    description: "Premium wireless headphones"
+  },
+  {
+    id: 4,
+    name: "Smart Watch",
+    price: 5000,
+    image: "./Images/image4.jpg",
+    description: "Feature-rich smartwatch"
+  },
+  {
+    id: 5,
+    name: "Tablet Ultra Pro",
+    price: 35000,
+    image: "./Images/image5.jpg",
+    description: "Versatile tablet for work and entertainment"
+  }
+];
